332d73fa6b1d3e5c37895997fbef7883236ab45e
Sanket <sanketanilkadam@gmail.com>
2022-04-05 19:16:37 +0530

    added palindrome

---
+++ recursion&backtracking/palindrome.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int pal(string s, int i)
+{
+    if (i >= s.length() / 2){
+        return true;
+    }
+        
+    if (s[i] != s[s.length() - i - 1])
+    {
+
+        return false;
+    }
+    else
+    {
+        return pal(s, i + 1);
+    }
+}
+
+int main()
+{
+    string s;
+
+    cin >> s;
+
+    cout<<pal(s, 0);
+    
+    return 0;
+}
