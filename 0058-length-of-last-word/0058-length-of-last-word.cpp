fe9a94d39aad8b53415f50af4e47f4ac984b4e9c
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2022-11-15 23:05:14 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (11.18%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        bool islastword=false;
+        int counter=0;
+        for(int i=s.length()-1;i>=0;i--){
+           
+            if(s[i]==' ' && counter>0){
+               return counter;
+            }
+            if(s[i]!=' '){
+                 counter++;
+            }
+           
+        }
+         return counter;
+    }
+   
+};
