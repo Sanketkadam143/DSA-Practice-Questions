f9d6afa2a110f4afa5204bdc5cd261d62bbcbc7c
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2022-12-02 17:57:37 +0530

    Time: 147 ms (9.32%), Space: 28.3 MB (48.41%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        int max=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i+=2){
+            max+=nums[i];          
+        }        
+        return max;
+    }
+};
