49b2e825073022410efbd1edfd7f9821b4560a73
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2022-12-02 18:13:20 +0530

    Time: 6 ms (8.95%), Space: 6 MB (17.81%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s=to_string(num);
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='6'){
+                s[i]='9';
+                break;
+            }
+        }
+       stringstream ss(s);
+       ss>>ans;
+       return ans;
+    }
+};
