dd3cff15bed036bb08f988dce0d1652e70f1f388
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2022-12-09 12:08:49 +0530

    Time: 11 ms (21.41%), Space: 8.6 MB (38.34%) - LeetHub

--- 0804-unique-morse-code-words/0804-unique-morse-code-words.cpp
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        string alphabets="abcdefghijklmnopqrstuvwxyz",temp="";
+        string temp="";
         vector<string>code={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        unordered_map<char,string>map;
         set<string>ans;
-
-        for(int i=0;i<26;i++){
-           map.insert({alphabets[i],code[i]});
-        }
         for(int i=0;i<words.size();i++){
             temp="";
             for(int j=0;j<words[i].length();j++){
-              temp+=map.at(words[i][j]);
+              temp+=code[words[i][j]-'a'];
             }
            
             ans.insert(temp);
