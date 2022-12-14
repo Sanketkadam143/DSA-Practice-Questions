4b85df4d1c08e827f1df349f8b0f04e6d714ffa1
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2022-12-14 20:11:30 +0530

    Time: 61 ms (15.04%), Space: 10.2 MB (14.51%) - LeetHub

---
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canBeIncreasing(vector<int>& nums) {
+        vector<int>temp;
+        for(int i=0;i<nums.size();i++){
+            temp=nums;
+            temp.erase(temp.begin()+i);
+            if(temp.size()==1)return true;
+            for(int j=1;j<temp.size();j++){
+                if(temp[j-1]>=temp[j]){
+                    break;
+                }else if(j==temp.size()-1){
+                    return true;
+                }
+                
+            }
+        }
+        return false;
+    }
+};
