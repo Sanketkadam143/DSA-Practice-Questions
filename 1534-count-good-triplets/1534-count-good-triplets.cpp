fdd2b1438e9cce98c8bb64dd5d0ebaaefe88b83b
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2022-12-19 12:03:33 +0530

    Time: 48 ms (81.20%), Space: 8.3 MB (22.79%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.cpp
+++ 1534-count-good-triplets/1534-count-good-triplets.cpp
@@ -8,7 +8,7 @@ class Solution
             {
                 for (int j = i + 1; j < arr.size(); j++)
                 {
-                  
+                    if (abs(arr[i] - arr[j]) <= a)
                         for (int k = j + 1; k < arr.size(); k++)
                         {
                             if (abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c)
