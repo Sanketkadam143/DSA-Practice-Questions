563412e93a9ed3abfe4d8e7b082b85a5c63c2502
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2023-01-03 11:42:38 +0530

    Time: 46 ms (42.37%), Space: 18.1 MB (23.33%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+     sort(nums.begin(),nums.end());
+     for(int i=0;i<=nums.size();i++){
+         if(i==nums.size()||i!=nums[i])return i;
+     }
+        return 0;
+    }
+};
