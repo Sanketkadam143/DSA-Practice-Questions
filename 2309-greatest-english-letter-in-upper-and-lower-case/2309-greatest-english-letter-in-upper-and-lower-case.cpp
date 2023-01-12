8c9316d04190c8575f46542187f56bb9176b55be
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2023-01-12 05:49:41 +0530

    Time: 22 ms (16.22%), Space: 7 MB (9.65%) - LeetHub

---
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string greatestLetter(string s) {
+        map<char,int>mp;
+        for(int i=0;i<s.length();i++){
+            mp[s[i]]++;
+        }
+        sort(s.begin(),s.end());
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            char l=tolower(s[i]);
+            char u=toupper(s[i]);
+            if(mp[l]&&mp[u])ans=u;
+        }
+        return ans;
+    }
+};
