72bdb7f93fbf21058a3e00a926ab93b83500655c
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2023-01-12 05:55:54 +0530

    Time: 12 ms (35.11%), Space: 6.9 MB (18.69%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -5,12 +5,15 @@ public:
         for(int i=0;i<s.length();i++){
             mp[s[i]]++;
         }
-        sort(s.begin(),s.end());
+        sort(s.begin(),s.end(),greater<char>());
         string ans="";
         for(int i=0;i<s.length();i++){
             char l=tolower(s[i]);
             char u=toupper(s[i]);
-            if(mp[l]&&mp[u])ans=u;
+            if(mp[l]&&mp[u]){
+                ans=u;
+                return ans;
+            };
         }
      return ans;
     }
