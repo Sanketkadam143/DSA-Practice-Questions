6264fc453cf97f190e4ead70651ca49841780040
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2023-01-14 10:09:34 +0530

    Time: 186 ms (19.80%), Space: 48.9 MB (5.03%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        int min=INT_MAX;
+        vector<vector<int>>ans;
+        map<int,int>mp;
+        sort(arr.begin(),arr.end());
+        for(int i=1;i<arr.size();i++){
+            if(abs(arr[i]-arr[i-1])<min)min=abs(arr[i]-arr[i-1]);
+        }
+        for(auto& num:arr)mp[num]++;
+        for(auto& map:mp){
+            if(mp.count(map.first+min))ans.push_back({map.first,map.first+min});
+        }
+     return ans;                                        
+    }
+};
