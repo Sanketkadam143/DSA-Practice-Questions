85e1079ce1f7cd54671bf7802e8cb27713a84c3a
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2023-03-02 09:55:16 +0530

    Time: 81 ms (58.18%), Space: 35.6 MB (55.00%) - LeetHub

---
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int hardestWorker(int n, vector<vector<int>>& logs) {
+        int longestTime = 0;
+        int hardestWorkerId = 0;
+        int prevLeaveTime = 0;
+        
+        for (int i = 0; i < logs.size(); i++) {
+            int currId = logs[i][0];
+            int currLeaveTime = logs[i][1];
+            int currTime = currLeaveTime - prevLeaveTime;
+            
+            if (currTime > longestTime) {
+                longestTime = currTime;
+                hardestWorkerId = currId;
+            }
+            else if (currTime == longestTime && currId < hardestWorkerId) {
+                hardestWorkerId = currId;
+            }
+            
+            prevLeaveTime = currLeaveTime;
+        }
+        
+        return hardestWorkerId;
+    }
+};
