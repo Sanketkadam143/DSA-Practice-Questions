08a0fe68c48308257f9c94424f1f040f3a54d654
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2023-04-18 19:29:14 +0530

    Time: 1085 ms (33.71%), Space: 29.5 MB (65.92%) - LeetHub

---
+++ 2644-find-the-maximum-divisibility-score/2644-find-the-maximum-divisibility-score.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    int maxDivScore(vector<int>& nums, vector<int>& divisors) {
+        int maxcount = 0;
+        int best_divisor = divisors[0];
+        for (int i = 0; i < divisors.size(); i++) {
+            int count = 0;
+            for (int j = 0; j < nums.size(); j++) {
+                if (nums[j] % divisors[i] == 0) {
+                    count++;
+                }
+            }
+            if(count==maxcount){
+               best_divisor = min(best_divisor,divisors[i]);
+            }
+            if (count > maxcount) {
+                maxcount = count;
+                best_divisor = divisors[i];
+            }
+        }
+        return best_divisor;
+    }
+};
