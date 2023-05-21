6425ef51fa7d4d0f3d918bc677628e7a09015b95
Sanket Kadam <83902364+Sanketkadam143@users.noreply.github.com>
2023-05-21 16:15:38 +0530

    Time: 31 ms (100.00%), Space: 15.1 MB (33.33%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int i=0,j=s.size()-1;
+        while(i<j){
+                if(s[i]<s[j]) s[j--]=s[i++];
+                else s[i++]=s[j--];
+        }
+        return s;
+    }
+};
